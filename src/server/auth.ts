99bec9430d3535343f2480957b93bff82c8dbccd
Belendor <dwarf11@gmail.com>
2023-04-19 09:37:54 +0300

    working gitlab and credentials auth in dockers

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,10 +5,9 @@ import {
   type DefaultSession,
 } from "next-auth";
 import GitlabProvider from "next-auth/providers/gitlab";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 import CredentialsProvider from "next-auth/providers/credentials"
+import { env } from "~/env.mjs";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -37,30 +36,8 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    jwt: ({ token, user }) => {
-      if(user) {
-        token.id = user.id
-      }
-      return token
-    },
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-    // session: ({ session, token }) => ({
-    //   if (token) {
-    //     session.user.id = token.id
-    //   }
-    //   return session
-
-    // }),
-  },
-  adapter: PrismaAdapter(prisma),
   providers: [
+    // OAuth authentication providers...
     GitlabProvider({
       name: "Magenta CICD",
       clientId: env.GITLAB_CLIENT_ID,
@@ -76,24 +53,14 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
+
+      
+      name: "Credentials",
       credentials: {
         username: { label: "Username", type: "text", placeholder: "" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
-      async authorize(credentials) { // req)
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-
+      async authorize(credentials, req) {
         if (credentials?.username === process.env.EMERGENCY_USERNAME && credentials?.password === process.env.EMERGENCY_PASSWORD) {
 
           const user = await prisma.user.findUnique({
@@ -104,41 +71,15 @@ export const authOptions: NextAuthOptions = {
           if (!user) {
             throw new Error("User not found");
           }
+          console.debug(user)
 
-          return { 
-            id: 2,
-            name: 'Emergency User', // placeholder
-            email: "emergency@email.com" // placeholder
-          }
+          return user
         }
 
         // login failed
         return null
-
-        const res = await fetch("/your/endpoint", {
-          method: 'POST',
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" }
-        })
-        const user = await res.json()
-  
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user
-        }
-        // Return null if user data could not be retrieved
-        return null
-      }
-    })
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+      },
+    }),
   ],
 };
 
